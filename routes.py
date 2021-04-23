56bd4f29edd410afbc7e6fb08cc0f31c25835bb3
Akash Ganguly <akashganguly880@gmail.com>
2021-04-23 11:53:16 +0530

    project setup

--- routes.py
+++ routes.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, request
 from auth import *
 route = Blueprint('route', __name__)
 
+#manage routing here
 route.add_url_rule('/login', methods=['POST'],view_func=login)
 route.add_url_rule('/signup', methods=['POST'],view_func=signup)
+route.add_url_rule('/reset', methods=['POST'],view_func=reset)
--- run.py
+++ run.py
@@ -2,28 +2,28 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:12345678@localhost/arrk'
 
 db = SQLAlchemy()
+
 
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.sqlite3'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+    # db init
     db.init_app(app)
 
+    # creates blueprint for app to be used across thew project
     from auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
+
     from routes import route as route_blueprint
     app.register_blueprint(route_blueprint)
 
     return app
 
+
 if __name__ == '__main__':
     app = create_app()
-
-    app.run(port="8085", debug=True)
+    app.run(port="8085", debug=True) # starting the app
